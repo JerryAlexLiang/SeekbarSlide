746f405ae1893e9dca4124eb810135b3d20cb1bd
liangyang <1935990239@qq.com>
2017-01-11 17:20:51 +0800

    SeekBar的使用、仿淘宝滑动验证

---
+++ app/src/main/java/com/liangyang/seekbarslide/MainActivity.java
@@ -0,0 +1,17 @@
+package com.liangyang.seekbarslide;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+/**
+ * 创建日期：2017/1/10 on 15:34
+ * 作者:杨亮 liangyang
+ * 描述:SeekBar的使用、仿淘宝滑动验证
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
